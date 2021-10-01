e225d90683a629df8351d8714cbefa9f5545fcff
Shaik Faizuddin 2 <shaikmohdfaizuddin@gmail.com>
2021-10-01 21:04:28 +0800

    event handler 2

--- event-handler/js/events.js
+++ event-handler/js/events.js
@@ -2,7 +2,21 @@ function transformUppercase(_val){
     return _val.toUppercase();
 }
 
-let elNamaPengguna = document.getELementaryById("NamaPengguna")
+let elNamaPengguna = document.getElementById("NamaPengguna")
 elNamaPengguna.addEventListener("keyup",function(){
     elNamaPengguna.value = transformUppercase(elNamaPengguna.value)
 })
+
+function checkPassword(){
+    let elKataLaluan = document.getElementById("kataLaluan")
+    let pass_val = elKataLaluan.value
+
+    if(pass_val.length > 5){
+        alert("Password check...OK!")
+    } else {
+        alert("Password too short!")
+    }
+}
+
+let elKL = document.getElementById("kataLaluan")
+elKL.onblur = checkPassword

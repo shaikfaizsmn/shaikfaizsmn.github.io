680515b4d16cf7ad2938d7d8ff57ca46b0e4a6e0
Shaik Faizuddin 2 <shaikmohdfaizuddin@gmail.com>
2021-10-04 22:49:13 +0800

    assignment test1

---
+++ assignment4/js/index.js
@@ -0,0 +1,19 @@
+console.clear()
+
+function mixMaxAvg(arr){
+    var max = arr[0]
+    var min = arr[0]
+    var sum = arr[0]
+    for (var i = 1; i < arr.length; i++){
+        if (arr[i] < min){
+            min = arr[i]
+        }
+        if (arr[i] > max){
+            max = arr[i]
+        } 
+        sum = sum + arr[i]
+    }
+    return [min, max, sum/arr.length]
+}
+
+console.log ('minMaxAvg')

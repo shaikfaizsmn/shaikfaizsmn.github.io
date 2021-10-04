9c7d1dbf7b6c7dd9aaa0348f459ce30f4e3dfa7d
Shaik Faizuddin 2 <shaikmohdfaizuddin@gmail.com>
2021-10-04 21:36:48 +0800

    seekbar

---
+++ video/js/index.js
@@ -0,0 +1,19 @@
+let video = document.getElementById("myVideo")
+
+let playBtn = document.getElementById("playBtn")
+playBtn.addEventListener("click", function(){
+    video.play()
+})
+
+let pauseBtn = document.getElementById("pauseBtn")
+pauseBtn.addEventListener("click",function(){
+    video.pause()
+})
+
+let freezeBtn = document.getElementById("freezeBtn")
+freezeBtn.addEventListener("mousedown",function(){
+    video.pause()
+})
+freezeBtn.addEventListener("mouseup",function(){
+    video.play()
+})

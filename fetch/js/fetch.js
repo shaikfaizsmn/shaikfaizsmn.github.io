98569ff8754617de013902d67eebf7782901161b
Shaik Faizuddin 2 <shaikmohdfaizuddin@gmail.com>
2021-10-06 21:16:42 +0800

    try je

--- fetch/js/fetch.js
+++ fetch/js/fetch.js
@@ -2,7 +2,7 @@ function LoadUser(){
     let url = "https://randomuser.me/api/"
 
     fetch(url)
-    .then(response) => response.json())
+    .then(response) = response.json()
     .then(data => {
         let fn = document.getElementById("firstName")
         let ln = document.getElementById("lastName")
@@ -20,3 +20,46 @@ let elLoadUser = document.getElementById("getRandomUser")
 elLoadUser.addEventListener("click", function(){
     LoadUser()
 })
+
+let elLoadMultipleUsers = document.getElementById("getMultipleUser")
+elLoadMultipleUsers.addEventListener("click",function(){
+    let userCount = document.getElementById("userCount")
+    elLoadMultipleUsers(userCount.value)
+})
+
+function elLoadMultipleUsers(userCount){
+    let url = "https://randomuser.me/api/?results=" + userCount
+    let temp = ""
+
+    fetch(url)
+    .then(response) = response.json()
+    .then(faiz => {
+        let allUsers = document.getElementById("allUsers")
+
+        for(let i = 0; i < data.results.length; i++){
+
+            let fn = '<div id="firstName">' + faiz.results[0].name.first + '</div>'
+            let ln = '<div>' + faiz.results[i].name.last + '</div>'
+            let phone = '<div>' + faiz.results[i].phone + '</div>'
+            let img = '<img src="' + faiz.results[i].picture.large + '">'
+
+        temp = temp + fn + ln + phone + img
+        }
+        allUsers.innerHTML = temp
+    })
+}
+
+let elLoadGender = document.getElementById("getGenderUser")
+elLoadGender.addEventListener("click",function(){
+    console.log("Load Gender Clicked")
+
+    let gender = document.getElementById("userGender")
+    console.log("Select Gender : " + gender.value)
+
+
+})
+
+function loadGender(gender, userCount){
+    let url = "https://randomuser.me/api/?results=" + userCount + "&gender=" + gender
+
+}

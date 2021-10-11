4ecbdf7734db4811f3ea1ea7ec6f9901976bfdda
Shaik Faizuddin 2 <shaikmohdfaizuddin@gmail.com>
2021-10-11 20:33:23 +0800

    bookjs commit

--- tableapp/js/book.js
+++ tableapp/js/book.js
@@ -1,5 +1,5 @@
 let bookNowBtn = document.getElementById("bookNow")
-bookNowBtn.addEventListener("click", function(){
+bookNowBtn.addEventListener("click", function () {
     let userName = document.getElementById("userName")
     let userNameVal = userName.value
 
@@ -12,12 +12,31 @@ bookNowBtn.addEventListener("click", function(){
     let userRemarks = document.getElementById("userRemarks")
     let userRemarksVal = userRemarks.value
 
-    bookNow(userNameVal, userEmailVal, userPaxVal, userRemarksVal)
+    BookNow(userNameVal, userEmailVal, userPaxVal, userRemarksVal)
 })
 
-function BookNow(userName, userEmail, userPax, userRemarks){
-    console.log(userName)
-    console.log(userEmail)
-    console.log(userPax)
-    console.log(userRemarks)
+function BookNow(userName, userEmail, userPax, userRemarks) {
+    let url = 'https://api.sheety.co/391933236f9693f6d5b117cf495aa9c8/bookingApp/bookings';
+    let body = {
+        booking: {
+            name: userName,
+            email: userEmail,
+            pax: userPax,
+            remarks: userRemarks
+
+        }
+    }
+    fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(body),
+        header: {
+            "Content-Type": "application/json"
+        }
+
+    })
+        .then((response) => response.json())
+        .then(json => {
+            // Do something with object
+            console.log(json.booking);
+        });
 }

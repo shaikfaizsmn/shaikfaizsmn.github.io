c83a3f14d0a29ad70df99bd98e75dc40e7619164
Shaik Faizuddin 2 <shaikmohdfaizuddin@gmail.com>
2021-10-15 22:01:15 +0800

    palindrome exc

--- assignment04/js/exc4.js
+++ assignment04/js/exc4.js
@@ -1,14 +1,22 @@
-var y = prompt("type number ");
-var x = input;
-
- function factorial(x) {
-   if(x==0) {
-      return 1;
-   }
-   return x * factorial(x-1);
-}
-
-function run(number) {
-    alert(factorial(parseInt(number, 10)));
-}
-run(x);
+function checkPalindrome(text)   
+{  
+    function checkPalindrome() {
+        var revStr = "";
+        var str = document.getElementById("str").value;
+        var i = str.length;
+        for (var j = i; j >= 0; j--) {
+          revStr = revStr + str.charAt(j);
+        }
+        if (str == revStr) {
+          alert("The entry is Palindrome");
+        } else {
+          alert("The entry is not a Palindrome");
+        }
+      }
+}  
+function checkPalindrome()  
+{  
+var num = document.getElementById("text").value;  
+var f = checkPalindrome(text);  
+document.getElementById("res").innerHTML="This word " + num + " is: " + f ;  
+}  
